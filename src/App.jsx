54540baa824b205efebc0f2669aab41d6fa30f16
Charan0413 <charanyadav417@gmail.com>
2025-05-27 16:40:10 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { USESTATE } from "react";
+import REACTLOGO from "./assets/react.svg";
+import VITELOGO from "/vite.svg";
 import "./App.css";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
